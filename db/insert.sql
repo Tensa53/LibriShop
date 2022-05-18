0b96d81a05f50bdc6ce3bcc34383e6e17c29c2d2
daniele <danieltensa53@protonmail.com>
2022-05-18 16:03:44 +0200

    Definito il database ed alcune query

--- db/insert.sql
+++ db/insert.sql
@@ -1,3 +1,9 @@
 /* In questo file inseriremo tutte le istruzioni relative all'inserimento dei record nelle varie tabelle,così da poter
    facilmente ripopolare il database sulle diverse macchine utilizzate
  */
+
+
+INSERT INTO LIBRO VALUES
+('1234567890123','LibroA','Un qualunque libro generico tanto per riempire il database e provare le nostre cose',4.60,'2020-05-06','EditoreA',0,30,),
+('0987654321098','LibroB','Un qualunque libro generico tanto per riempire il database e provare le nostre cose',3.20,'2019-07-02','EditoreB',0,20,),
+('1122334455666','LibroD','Un qualunque libro generico tanto per riempire il database e provare le nostre cose',6.90,'2021-08-03','EditoreD',0,10,);
--- db/query.sql
+++ db/query.sql
@@ -1,1 +1,16 @@
 -- In questo file inseriremo tutte le interrogazioni al database che ci saranno poi utili nei relativi model
+
+-- Seleziona tutti i libri
+SELECT *
+FROM Libro; --usare LIMIT per restringere le righe di output
+
+-- Seleziona il libro per genere
+SELECT *
+FROM Libro L, Genere G, Appartenenza A
+WHERE (A.IsbnLibro = L.ISBN AND A.Genere = G.Nome) AND G.Nome = 'Inserisci nome genere';
+
+-- Componi il prodotto del Carrello
+SELECT L.Disponibilita,L.Nome,D.Quantita,D.Prezzo,sum(D.Prezzo)
+FROM Libro L,Dettaglio D,Carrello C
+WHERE Dettaglio.ISBNLibro = L.ISBN AND D.Carrello = C.ID
+GROUP BY (L.Disponibilita,L.Nome,D.Quantita,D.Prezzo,sum(D.Prezzo),C.ID);
