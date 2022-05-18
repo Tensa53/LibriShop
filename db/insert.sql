9a0bb49ad6914b17ed9983de081f0729ecf8bb2f
daniele <danieletensa53@protonmail.com>
2022-05-18 23:36:12 +0200

    Aggiornati i file del db

--- db/insert.sql
+++ db/insert.sql
@@ -2,8 +2,9 @@
    facilmente ripopolare il database sulle diverse macchine utilizzate
  */
 
+use base;
 
-INSERT INTO LIBRO VALUES
-('1234567890123','LibroA','Un qualunque libro generico tanto per riempire il database e provare le nostre cose',4.60,'2020-05-06','EditoreA',0,30,),
-('0987654321098','LibroB','Un qualunque libro generico tanto per riempire il database e provare le nostre cose',3.20,'2019-07-02','EditoreB',0,20,),
-('1122334455666','LibroD','Un qualunque libro generico tanto per riempire il database e provare le nostre cose',6.90,'2021-08-03','EditoreD',0,10,);
+INSERT INTO Libro VALUES
+('1234567890123','LibroA','Un qualunque libro generico tanto per riempire il database e provare le nostre cose',4.60,'2020-05-06','EditoreA',0,30,'./img/LibroA.jpg'),
+('0987654321098','LibroB','Un qualunque libro generico tanto per riempire il database e provare le nostre cose',3.20,'2019-07-02','EditoreB',0,20,'./img/LibroB.jpg'),
+('1122334455666','LibroD','Un qualunque libro generico tanto per riempire il database e provare le nostre cose',6.90,'2021-08-03','EditoreD',0,10,'./img/LibroD.jpg');
--- db/tabelle.sql
+++ db/tabelle.sql
@@ -7,120 +7,120 @@ USE base;
 -- tabelle indipendenti
 
 CREATE TABLE Libro (
-                       ISBN char(13) primary key,
-                       Titolo varchar(30) not null, /*nel titolo viene eventualmente specificata l'edizione*/
-                       Descrizione varchar(500) not null,
-                       Prezzo float not null,
-                       DataPubblicazione date not null,
-                       Editore varchar(20) not null,
-                       Sconto int not null,
-                       Disponibilita int not null,
-                       Foto varchar(50) not null
+ISBN char(13) primary key,
+Titolo varchar(30) not null, /*nel titolo viene eventualmente specificata l'edizione*/
+Descrizione varchar(500) not null,
+Prezzo float not null,
+DataPubblicazione date not null,
+Editore varchar(20) not null,
+Sconto int not null,
+Disponibilita int not null,
+Foto varchar(50) not null
 );
 
 CREATE TABLE Genere (
-    Nome varchar(20) primary key
+Nome varchar(20) primary key
 );
 
 CREATE TABLE Autore (
-                        CF char(16) primary key,
-                        Nome varchar(20) not null,
-                        Cognome varchar(20) not null
+CF char(16) primary key,
+Nome varchar(20) not null,
+Cognome varchar(20) not null
 );
 
 CREATE TABLE Utente (
-                        Email varchar(30) primary key,
-                        Username varchar(20) unique,
-                        Nome varchar(20) not null,
-                        Cognome varchar(20) not null,
-                        PasswordHash varchar(40) not null
+Email varchar(30) primary key,
+Username varchar(20) unique,
+Nome varchar(20) not null,
+Cognome varchar(20) not null,
+PasswordHash varchar(40) not null
 );
 
 CREATE TABLE Indirizzo (
-                           Via varchar(40) not null,
-                           Civico varchar(5) not null,
-                           CAP char(5) not null,
-                           Citta varchar(20) not null,
-                           Provincia varchar(20) not null,
-                           Stato varchar(20) not null,
-                           primary key(Via,Civico,CAP)
+Via varchar(40) not null,
+Civico varchar(5) not null,
+CAP char(5) not null,
+Citta varchar(20) not null,
+Provincia varchar(20) not null,
+Stato varchar(20) not null,
+primary key(Via,Civico,CAP)
 );
 
 CREATE TABLE Pagamento (
-                           NumeroCarta varchar(16) primary key,
-                           Scadenza date not null,
-                           CCV char(3) not null
+NumeroCarta varchar(16) primary key,
+Scadenza date not null,
+CCV char(3) not null
 );
 
 -- tabelle dipendenti
 
 CREATE TABLE Appartenenza (
-                              ISBNLibro char(13),
-                              Genere varchar(20),
-                              primary key(ISBNLibro,Genere),
-                              foreign key (ISBNLibro) references Libro(ISBN),
-                              foreign key (Genere) references Genere(Nome)
+ISBNLibro char(13),
+Genere varchar(20),
+primary key(ISBNLibro,Genere),
+foreign key (ISBNLibro) references Libro(ISBN),
+foreign key (Genere) references Genere(Nome)
 );
 
 CREATE TABLE Scrittura (
-                           ISBNLibro char(13),
-                           Autore char(16),
-                           primary key(ISBNLibro,Autore),
-                           foreign key(ISBNLibro) references Libro(ISBN),
-                           foreign key(Autore) references Autore(CF)
+ISBNLibro char(13),
+Autore char(16),
+primary key(ISBNLibro,Autore),
+foreign key(ISBNLibro) references Libro(ISBN),
+foreign key(Autore) references Autore(CF)
 );
 
 CREATE TABLE Ordine(
-                       ID int primary key auto_increment,
-                       DataOrdine date not null,
-                       Via varchar(40) not null,
-                       Numero varchar(5) not null,
-                       CAP char(5) not null,
-                       Citta varchar(20) not null,
-                       Provincia varchar(20) not null,
-                       Stato varchar(20) not null,
-                       NumeroCarta varchar(16) not null,
-                       Scadenza date not null,
-                       CCV char(3) not null,
-                       Totale float not null,
-                       Utente varchar(30) not null,
-                       foreign key(Utente) references Utente(Email)
+ID int primary key auto_increment,
+DataOrdine date not null,
+Via varchar(40) not null,
+Numero varchar(5) not null,
+CAP char(5) not null,
+Citta varchar(20) not null,
+Provincia varchar(20) not null,
+Stato varchar(20) not null,
+NumeroCarta varchar(16) not null,
+Scadenza date not null,
+CCV char(3) not null,
+Totale float not null,
+Utente varchar(30) not null,
+foreign key(Utente) references Utente(Email)
 );
 
 CREATE TABLE Carrello (
-                          ID int primary key auto_increment,
-                          Utente varchar(30) not null,
-                          Totale float not null,
-                          foreign key(Utente) references Utente(Email)
+ID int primary key auto_increment,
+Utente varchar(30) not null,
+Totale float not null,
+foreign key(Utente) references Utente(Email)
 );
 
 CREATE TABLE Dettaglio (
-                           ID int primary key auto_increment,
-                           Quantita int not null,
-                           Prezzo float not null,
-                           Carrello int,
-                           Ordine int,
-                           ISBNLibro varchar(13),
-                           foreign key (Carrello) references Carrello(ID),
-                           foreign key (Ordine) references Ordine(ID)
+ID int primary key auto_increment,
+Quantita int not null,
+Prezzo float not null,
+Carrello int,
+Ordine int,
+ISBNLibro varchar(13),
+foreign key (Carrello) references Carrello(ID),
+foreign key (Ordine) references Ordine(ID)
 );
 
 CREATE TABLE Dichiarazione (
-                               Utente varchar(30),
-                               IndirizzoVia varchar(40),
-                               IndirizzoNumero varchar(5),
-                               IndirizzoCAP char(5),
-                               primary key(Utente,IndirizzoVia,IndirizzoNumero,IndirizzoCAP),
-                               foreign key (Utente) references Utente(Email),
-                               foreign key(IndirizzoVia,IndirizzoNumero,IndirizzoCAP) references Indirizzo(Via,Civico,CAP)
+Utente varchar(30),
+IndirizzoVia varchar(40),
+IndirizzoNumero varchar(5),
+IndirizzoCAP char(5),
+primary key(Utente,IndirizzoVia,IndirizzoNumero,IndirizzoCAP),
+foreign key (Utente) references Utente(Email),
+foreign key(IndirizzoVia,IndirizzoNumero,IndirizzoCAP) references Indirizzo(Via,Civico,CAP)
 );
 
 CREATE TABLE Definizione (
-                             Utente varchar(30),
-                             Pagamento varchar(16),
-                             primary key(Utente,Pagamento),
-                             foreign key(Utente) references Utente(Email),
-                             foreign key(Pagamento) references Pagamento(NumeroCarta)
+Utente varchar(30),
+Pagamento varchar(16),
+primary key(Utente,Pagamento),
+foreign key(Utente) references Utente(Email),
+foreign key(Pagamento) references Pagamento(NumeroCarta)
 );
 
 
