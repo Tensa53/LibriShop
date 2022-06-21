a0130e2662a695e8ff00bf9afb71c4471ec3aaf5
daniele <danieletensa53@protonmail.com>
2022-06-21 14:27:51 +0200

    Implementazione iniziale dell'inserimento di un utente (parte admin)

---
+++ src/main/java/com/example/progettotsw/controller/riservata/admin/InserisciUtenteServlet.java
@@ -0,0 +1,70 @@
+package com.example.progettotsw.controller.riservata.admin;
+
+import com.example.progettotsw.model.CarrelloDAO;
+import com.example.progettotsw.model.Utente;
+import com.example.progettotsw.model.UtenteDAO;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/inserisci-utente")
+public class InserisciUtenteServlet extends HttpServlet {
+    public void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException, ServletException {
+        Utente utente = (Utente) request.getSession().getAttribute("utente");
+
+        if (utente != null) {
+            if (utente.isAmministratore()) {
+                String mail = request.getParameter("mailr");
+                String username = request.getParameter("usernamer");
+                String nome = request.getParameter("nomer");
+                String cognome = request.getParameter("cognomer");
+                String password = request.getParameter("passwordr");
+                boolean amministratore = Boolean.parseBoolean(request.getParameter("amministratorer"));
+
+                if(mail != null && username != null && nome != null && cognome != null && password != null) {
+                    Utente nuovoUtente = new Utente(mail,username,nome,cognome,amministratore);
+                    nuovoUtente.setPassword(password);
+
+                    UtenteDAO utenteDAO = new UtenteDAO();
+
+                    int row = utenteDAO.doSave(nuovoUtente);
+
+                    String msg = null;
+
+                    if (row == 1)
+                        msg = "Inserimento effettuato con successo !!! Torna alla <a href = \"http://localhost:8080/progettoTSW_war_exploded/area-riservata\"> dashboard </a> oppure effettua un altro inserimento";
+
+                    request.setAttribute("msg",msg);
+                }
+
+                if (!amministratore) {
+                    CarrelloDAO carrelloDAO = new CarrelloDAO();
+
+                    carrelloDAO.doCreate(mail);
+                }
+
+                String address = "/WEB-INF/ADMIN/insUtente.jsp";
+
+                RequestDispatcher rd = request.getRequestDispatcher(address);
+
+                rd.forward(request,response);
+
+            } else
+                response.sendRedirect("http://localhost:8080/progettoTSW_war_exploded/home");
+        } else
+            response.sendRedirect("http://localhost:8080/progettoTSW_war_exploded/home");
+
+
+
+
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        doPost(request,response);
+    }
+}
---
+++ src/main/webapp/WEB-INF/ADMIN/insUtente.jsp
@@ -0,0 +1,51 @@
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Inserisci Utente</title>
+    <link rel="stylesheet" type="text/css" href="./css/stile.css">
+    <script src="./script/mail.js" type="text/javascript"></script>
+    <script src="./script/username.js" type="text/javascript"></script>
+    <%String msg = (String) request.getAttribute("msg");%>
+</head>
+<body>
+
+<jsp:include page="../INCLUDE/header.jsp"></jsp:include>
+
+<jsp:include page="../INCLUDE/nav.jsp"></jsp:include>
+
+<body>
+<div id="container-registrazione">
+    <form name="Registrazione" action="inserisci-utente" method="post" onsubmit="return validateFormRegistrazione()">
+        <label for = "nome">Nome : </label> <br>
+        <input type="text" name="nomer" id="nome" required><br>
+        <label for = "cognome">Cognome : </label> <br>
+        <input type="text" name="cognomer" id="cognome" required><br>
+        <label for = "controlla-mail">Mail : </label> <br>
+        <p id="controllomail"></p>
+        <input type="email" name="mailr" id="controlla-mail" onblur="ControllaMail()" required><br>
+        <label for = "controlla-username">Username : </label><br>
+        <p id="controllousername"></p>
+        <input type="text" name="usernamer" id="controlla-username" onblur="ControllaUsername()" required><br>
+        <label for = "password">Password : </label>
+        <p>(La password deve contenere almeno 8 caratteri di cui almeno uno maiuscolo, un carattere speciale, un numero.)</p>
+        <input type="password" name="passwordr" id="password" pattern="(?=.*[!@#$%^&*])(?=.*\d)(?=.*[A-Z]).{8,}" required><br>
+        <label for="amministratore">Amministratore : </label><br>
+        <select id="amministratore" name="amministratorer">
+            <option value="true">SI</option>
+            <option value="false">NO</option>
+        </select><br>
+        <input type="submit" value="Inserisci">
+    </form>
+</div>
+
+
+<% if(msg != null) {%>
+<p class="green">${msg}</p>
+<%}%>
+
+<jsp:include page="../INCLUDE/footer.jsp"></jsp:include>
+
+
+</body>
+</html>
